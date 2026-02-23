026660c78d97894bfbf6b87fad766cfca0f2954a
Dominic Gannaway <dg@domgan.com>
2026-02-23 22:15:17 +0000

    more failing tests

---
+++ packages/ripple/tests/hydration/compiled/server/nested-control-flow.js
@@ -0,0 +1,688 @@
+// @ts-nocheck
+import * as _$_ from 'ripple/internal/server';
+
+export function ForIf(__output) {
+	_$_.push_component();
+
+	const items = [
+		{ id: 1, show: true, label: 'One' },
+		{ id: 2, show: true, label: 'Two' },
+		{ id: 3, show: false, label: 'Three' }
+	];
+
+	__output.push('<ul');
+	__output.push(' class="for-if"');
+	__output.push('>');
+
+	{
+		__output.push('<!--[-->');
+
+		for (const item of items) {
+			__output.push('<!--[-->');
+
+			if (_$_.get_property(item, 'show')) {
+				__output.push('<li');
+				__output.push(_$_.attr('class', `item item-${item.id}`));
+				__output.push('>');
+
+				{
+					__output.push(_$_.escape(item.label));
+				}
+
+				__output.push('</li>');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('<!--]-->');
+	}
+
+	__output.push('</ul>');
+	_$_.pop_component();
+}
+
+export function ForSwitch(__output) {
+	_$_.push_component();
+
+	const items = [
+		{ id: 1, kind: 'a' },
+		{ id: 2, kind: 'b' },
+		{ id: 3, kind: 'a' }
+	];
+
+	__output.push('<ul');
+	__output.push(' class="for-switch"');
+	__output.push('>');
+
+	{
+		__output.push('<!--[-->');
+
+		for (const item of items) {
+			__output.push('<!--[-->');
+
+			switch (item.kind) {
+				case 'a':
+					__output.push('<li');
+					__output.push(_$_.attr('class', `item item-${item.id} kind-a`));
+					__output.push('>');
+					{
+						__output.push(_$_.escape(`A-${item.id}`));
+					}
+					__output.push('</li>');
+					break;
+
+				default:
+					__output.push('<li');
+					__output.push(_$_.attr('class', `item item-${item.id} kind-b`));
+					__output.push('>');
+					{
+						__output.push(_$_.escape(`B-${item.id}`));
+					}
+					__output.push('</li>');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('<!--]-->');
+	}
+
+	__output.push('</ul>');
+	_$_.pop_component();
+}
+
+export function IfSwitch(__output) {
+	_$_.push_component();
+
+	const show = true;
+	const kind = 'a';
+
+	__output.push('<div');
+	__output.push(' class="if-switch"');
+	__output.push('>');
+
+	{
+		__output.push('<!--[-->');
+
+		if (show) {
+			__output.push('<!--[-->');
+
+			switch (kind) {
+				case 'a':
+					__output.push('<p');
+					__output.push(' class="case-a"');
+					__output.push('>');
+					{
+						__output.push('Case A');
+					}
+					__output.push('</p>');
+					break;
+
+				default:
+					__output.push('<p');
+					__output.push(' class="case-default"');
+					__output.push('>');
+					{
+						__output.push('Default');
+					}
+					__output.push('</p>');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('<!--]-->');
+	}
+
+	__output.push('</div>');
+	_$_.pop_component();
+}
+
+export function IfSwitchHidden(__output) {
+	_$_.push_component();
+
+	const show = false;
+	const kind = 'a';
+
+	__output.push('<div');
+	__output.push(' class="if-switch-hidden"');
+	__output.push('>');
+
+	{
+		__output.push('<!--[-->');
+
+		if (show) {
+			__output.push('<!--[-->');
+
+			switch (kind) {
+				case 'a':
+					__output.push('<p');
+					__output.push(' class="case-a"');
+					__output.push('>');
+					{
+						__output.push('Case A');
+					}
+					__output.push('</p>');
+					break;
+
+				default:
+					__output.push('<p');
+					__output.push(' class="case-default"');
+					__output.push('>');
+					{
+						__output.push('Default');
+					}
+					__output.push('</p>');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('<!--]-->');
+		__output.push('<p');
+		__output.push(' class="after"');
+		__output.push('>');
+
+		{
+			__output.push('after');
+		}
+
+		__output.push('</p>');
+	}
+
+	__output.push('</div>');
+	_$_.pop_component();
+}
+
+export function ForIfSwitchSingle(__output) {
+	_$_.push_component();
+
+	const items = [{ id: 1, kind: 'a', show: true }];
+
+	__output.push('<ul');
+	__output.push(' class="for-if-switch-single"');
+	__output.push('>');
+
+	{
+		__output.push('<!--[-->');
+
+		for (const item of items) {
+			__output.push('<!--[-->');
+
+			if (_$_.get_property(item, 'show')) {
+				__output.push('<!--[-->');
+
+				switch (item.kind) {
+					case 'a':
+						__output.push('<li');
+						__output.push(_$_.attr('class', `item item-${item.id} kind-a`));
+						__output.push('>');
+						{
+							__output.push(_$_.escape(`A-${item.id}`));
+						}
+						__output.push('</li>');
+						break;
+
+					default:
+						__output.push('<li');
+						__output.push(_$_.attr('class', `item item-${item.id} kind-default`));
+						__output.push('>');
+						{
+							__output.push(_$_.escape(`D-${item.id}`));
+						}
+						__output.push('</li>');
+				}
+
+				__output.push('<!--]-->');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('<!--]-->');
+	}
+
+	__output.push('</ul>');
+	_$_.pop_component();
+}
+
+export function ForIfSwitchMulti(__output) {
+	_$_.push_component();
+
+	const items = [
+		{ id: 1, kind: 'a', show: true },
+		{ id: 2, kind: 'b', show: true }
+	];
+
+	__output.push('<ul');
+	__output.push(' class="for-if-switch-multi"');
+	__output.push('>');
+
+	{
+		__output.push('<!--[-->');
+
+		for (const item of items) {
+			__output.push('<!--[-->');
+
+			if (_$_.get_property(item, 'show')) {
+				__output.push('<!--[-->');
+
+				switch (item.kind) {
+					case 'a':
+						__output.push('<li');
+						__output.push(_$_.attr('class', `item item-${item.id} kind-a`));
+						__output.push('>');
+						{
+							__output.push(_$_.escape(`A-${item.id}`));
+						}
+						__output.push('</li>');
+						break;
+
+					default:
+						__output.push('<li');
+						__output.push(_$_.attr('class', `item item-${item.id} kind-b`));
+						__output.push('>');
+						{
+							__output.push(_$_.escape(`B-${item.id}`));
+						}
+						__output.push('</li>');
+				}
+
+				__output.push('<!--]-->');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('<!--]-->');
+	}
+
+	__output.push('</ul>');
+	_$_.pop_component();
+}
+
+export function ForIfSwitchWithDisabled(__output) {
+	_$_.push_component();
+
+	const items = [
+		{ id: 1, kind: 'a', show: true },
+		{ id: 2, kind: 'b', show: false },
+		{ id: 3, kind: 'a', show: true }
+	];
+
+	__output.push('<ul');
+	__output.push(' class="for-if-switch-disabled"');
+	__output.push('>');
+
+	{
+		__output.push('<!--[-->');
+
+		for (const item of items) {
+			__output.push('<!--[-->');
+
+			if (_$_.get_property(item, 'show')) {
+				__output.push('<!--[-->');
+
+				switch (item.kind) {
+					case 'a':
+						__output.push('<li');
+						__output.push(_$_.attr('class', `item item-${item.id} kind-a`));
+						__output.push('>');
+						{
+							__output.push(_$_.escape(`A-${item.id}`));
+						}
+						__output.push('</li>');
+						break;
+
+					default:
+						__output.push('<li');
+						__output.push(_$_.attr('class', `item item-${item.id} kind-b`));
+						__output.push('>');
+						{
+							__output.push(_$_.escape(`B-${item.id}`));
+						}
+						__output.push('</li>');
+				}
+
+				__output.push('<!--]-->');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('<!--]-->');
+	}
+
+	__output.push('</ul>');
+	_$_.pop_component();
+}
+
+export async function SwitchTry(__output) {
+	return _$_.async(async () => {
+		_$_.push_component();
+
+		const kind = 'a';
+
+		__output.push('<div');
+		__output.push(' class="switch-try"');
+		__output.push('>');
+
+		{
+			__output.push('<!--[-->');
+
+			switch (kind) {
+				case 'a':
+					__output.push('<p');
+					__output.push(' class="pending-a"');
+					__output.push('>');
+					{
+						__output.push('A pending');
+					}
+					__output.push('</p>');
+					await _$_.async(async () => {
+						__output.push('<p');
+						__output.push(' class="resolved-a"');
+						__output.push('>');
+
+						{
+							__output.push('A resolved');
+						}
+
+						__output.push('</p>');
+					});
+					break;
+
+				default:
+					__output.push('<p');
+					__output.push(' class="default"');
+					__output.push('>');
+					{
+						__output.push('Default');
+					}
+					__output.push('</p>');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('</div>');
+		_$_.pop_component();
+	});
+}
+
+export async function ForSwitchTry(__output) {
+	return _$_.async(async () => {
+		_$_.push_component();
+
+		const items = [{ id: 1, kind: 'a' }, { id: 2, kind: 'b' }];
+
+		__output.push('<ul');
+		__output.push(' class="for-switch-try"');
+		__output.push('>');
+
+		{
+			__output.push('<!--[-->');
+
+			for (const item of items) {
+				__output.push('<!--[-->');
+
+				switch (item.kind) {
+					case 'a':
+						__output.push('<li');
+						__output.push(_$_.attr('class', `pending pending-${item.id}`));
+						__output.push('>');
+						{
+							__output.push(_$_.escape(`pending ${item.id}`));
+						}
+						__output.push('</li>');
+						await _$_.async(async () => {
+							__output.push('<li');
+							__output.push(_$_.attr('class', `item item-${item.id} kind-a`));
+							__output.push('>');
+
+							{
+								__output.push(_$_.escape(`A-${item.id}`));
+							}
+
+							__output.push('</li>');
+						});
+						break;
+
+					default:
+						__output.push('<li');
+						__output.push(_$_.attr('class', `pending pending-${item.id}`));
+						__output.push('>');
+						{
+							__output.push(_$_.escape(`pending ${item.id}`));
+						}
+						__output.push('</li>');
+						await _$_.async(async () => {
+							__output.push('<li');
+							__output.push(_$_.attr('class', `item item-${item.id} kind-b`));
+							__output.push('>');
+
+							{
+								__output.push(_$_.escape(`B-${item.id}`));
+							}
+
+							__output.push('</li>');
+						});
+				}
+
+				__output.push('<!--]-->');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('</ul>');
+		_$_.pop_component();
+	});
+}
+
+export async function ForIfTry(__output) {
+	return _$_.async(async () => {
+		_$_.push_component();
+
+		const items = [{ id: 1, show: true }, { id: 2, show: true }];
+
+		__output.push('<ul');
+		__output.push(' class="for-if-try"');
+		__output.push('>');
+
+		{
+			__output.push('<!--[-->');
+
+			for (const item of items) {
+				__output.push('<!--[-->');
+
+				if (_$_.get_property(item, 'show')) {
+					__output.push('<li');
+					__output.push(_$_.attr('class', `pending pending-${item.id}`));
+					__output.push('>');
+
+					{
+						__output.push(_$_.escape(`pending ${item.id}`));
+					}
+
+					__output.push('</li>');
+
+					await _$_.async(async () => {
+						__output.push('<li');
+						__output.push(_$_.attr('class', `item item-${item.id}`));
+						__output.push('>');
+
+						{
+							__output.push(_$_.escape(`item-${item.id}`));
+						}
+
+						__output.push('</li>');
+					});
+				}
+
+				__output.push('<!--]-->');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('</ul>');
+		_$_.pop_component();
+	});
+}
+
+export async function ForIfSwitchTrySingle(__output) {
+	return _$_.async(async () => {
+		_$_.push_component();
+
+		const items = [{ id: 1, kind: 'a', show: true }];
+
+		__output.push('<ul');
+		__output.push(' class="for-if-switch-try-single"');
+		__output.push('>');
+
+		{
+			__output.push('<!--[-->');
+
+			for (const item of items) {
+				__output.push('<!--[-->');
+
+				if (_$_.get_property(item, 'show')) {
+					__output.push('<!--[-->');
+
+					switch (item.kind) {
+						case 'a':
+							__output.push('<li');
+							__output.push(_$_.attr('class', `pending pending-${item.id}`));
+							__output.push('>');
+							{
+								__output.push(_$_.escape(`pending ${item.id}`));
+							}
+							__output.push('</li>');
+							await _$_.async(async () => {
+								__output.push('<li');
+								__output.push(_$_.attr('class', `item item-${item.id} kind-a`));
+								__output.push('>');
+
+								{
+									__output.push(_$_.escape(`A-${item.id}`));
+								}
+
+								__output.push('</li>');
+							});
+							break;
+
+						default:
+							__output.push('<li');
+							__output.push(_$_.attr('class', `pending pending-${item.id}`));
+							__output.push('>');
+							{
+								__output.push(_$_.escape(`pending ${item.id}`));
+							}
+							__output.push('</li>');
+							await _$_.async(async () => {
+								__output.push('<li');
+								__output.push(_$_.attr('class', `item item-${item.id} kind-default`));
+								__output.push('>');
+
+								{
+									__output.push(_$_.escape(`D-${item.id}`));
+								}
+
+								__output.push('</li>');
+							});
+					}
+
+					__output.push('<!--]-->');
+				}
+
+				__output.push('<!--]-->');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('</ul>');
+		_$_.pop_component();
+	});
+}
+
+export async function ForIfSwitchTryMulti(__output) {
+	return _$_.async(async () => {
+		_$_.push_component();
+
+		const items = [
+			{ id: 1, kind: 'a', show: true },
+			{ id: 2, kind: 'b', show: true }
+		];
+
+		__output.push('<ul');
+		__output.push(' class="for-if-switch-try-multi"');
+		__output.push('>');
+
+		{
+			__output.push('<!--[-->');
+
+			for (const item of items) {
+				__output.push('<!--[-->');
+
+				if (_$_.get_property(item, 'show')) {
+					__output.push('<!--[-->');
+
+					switch (item.kind) {
+						case 'a':
+							__output.push('<li');
+							__output.push(_$_.attr('class', `pending pending-${item.id}`));
+							__output.push('>');
+							{
+								__output.push(_$_.escape(`pending ${item.id}`));
+							}
+							__output.push('</li>');
+							await _$_.async(async () => {
+								__output.push('<li');
+								__output.push(_$_.attr('class', `item item-${item.id} kind-a`));
+								__output.push('>');
+
+								{
+									__output.push(_$_.escape(`A-${item.id}`));
+								}
+
+								__output.push('</li>');
+							});
+							break;
+
+						default:
+							__output.push('<li');
+							__output.push(_$_.attr('class', `pending pending-${item.id}`));
+							__output.push('>');
+							{
+								__output.push(_$_.escape(`pending ${item.id}`));
+							}
+							__output.push('</li>');
+							await _$_.async(async () => {
+								__output.push('<li');
+								__output.push(_$_.attr('class', `item item-${item.id} kind-b`));
+								__output.push('>');
+
+								{
+									__output.push(_$_.escape(`B-${item.id}`));
+								}
+
+								__output.push('</li>');
+							});
+					}
+
+					__output.push('<!--]-->');
+				}
+
+				__output.push('<!--]-->');
+			}
+
+			__output.push('<!--]-->');
+		}
+
+		__output.push('</ul>');
+		_$_.pop_component();
+	});
+}
---
+++ packages/ripple/tests/hydration/nested-control-flow.test.js
@@ -0,0 +1,156 @@
+/**
+ * Granular hydration tests for nested control flow combinations.
+ *
+ * Each test isolates one specific control flow pairing so failures
+ * point directly at the responsible block type. Progression:
+ *
+ *   for+if  →  for+switch  →  if+switch  →  for+if+switch (single/multi/disabled)
+ *   →  switch+try (baseline)
+ *   →  for+switch+try  →  for+if+try  →  for+if+switch+try (single then multi)
+ *
+ * The try-containing variants mirror MixedControlFlowStatic exactly.
+ */
+import { describe, it, expect } from 'vitest';
+import { hydrateComponent, container, stripHydrationMarkers } from '../setup-hydration.js';
+
+import * as ServerComponents from './compiled/server/nested-control-flow.js';
+import * as ClientComponents from './compiled/client/nested-control-flow.js';
+
+describe('hydration > nested control flow (granular)', () => {
+	// ── for + if ────────────────────────────────────────────────────────────────
+	describe('for + if', () => {
+		it('hydrates 3-item list where 2 are shown and 1 is hidden', async () => {
+			await hydrateComponent(ServerComponents.ForIf, ClientComponents.ForIf);
+
+			const items = Array.from(container.querySelectorAll('.item')).map((n) => n.textContent);
+			expect(items).toEqual(['One', 'Two']);
+			expect(container.querySelector('.item-3')).toBeNull();
+		});
+	});
+
+	// ── for + switch ─────────────────────────────────────────────────────────────
+	describe('for + switch', () => {
+		it('hydrates 3-item list where each item uses a switch', async () => {
+			await hydrateComponent(ServerComponents.ForSwitch, ClientComponents.ForSwitch);
+
+			const items = Array.from(container.querySelectorAll('.item')).map((n) => n.textContent);
+			expect(items).toEqual(['A-1', 'B-2', 'A-3']);
+			expect(container.querySelector('.kind-a.item-1')).not.toBeNull();
+			expect(container.querySelector('.kind-b.item-2')).not.toBeNull();
+			expect(container.querySelector('.kind-a.item-3')).not.toBeNull();
+		});
+	});
+
+	// ── if + switch (no for) ─────────────────────────────────────────────────────
+	describe('if + switch', () => {
+		it('hydrates an if wrapping a switch when the condition is true', async () => {
+			await hydrateComponent(ServerComponents.IfSwitch, ClientComponents.IfSwitch);
+
+			expect(container.querySelector('.case-a')?.textContent).toBe('Case A');
+			expect(container.querySelector('.case-default')).toBeNull();
+		});
+
+		it('hydrates an if wrapping a switch when the condition is false', async () => {
+			await hydrateComponent(ServerComponents.IfSwitchHidden, ClientComponents.IfSwitchHidden);
+
+			expect(container.querySelector('.case-a')).toBeNull();
+			// Sibling node after the if+switch must still hydrate correctly
+			expect(container.querySelector('.after')?.textContent).toBe('after');
+		});
+	});
+
+	// ── for + if + switch (single item — no inter-item cursor advance) ────────────
+	describe('for + if + switch (single item)', () => {
+		it('hydrates a single-item for+if+switch without cursor advance between items', async () => {
+			await hydrateComponent(
+				ServerComponents.ForIfSwitchSingle,
+				ClientComponents.ForIfSwitchSingle,
+			);
+
+			const items = Array.from(container.querySelectorAll('.item')).map((n) => n.textContent);
+			expect(items).toEqual(['A-1']);
+			expect(container.querySelector('.item-1.kind-a')).not.toBeNull();
+		});
+	});
+
+	// ── for + if + switch (multiple items — the regression) ──────────────────────
+	describe('for + if + switch (multiple items)', () => {
+		it('hydrates a two-item for+if+switch (cursor must advance correctly between items)', async () => {
+			await hydrateComponent(ServerComponents.ForIfSwitchMulti, ClientComponents.ForIfSwitchMulti);
+
+			const items = Array.from(container.querySelectorAll('.item')).map((n) => n.textContent);
+			expect(items).toEqual(['A-1', 'B-2']);
+			expect(container.querySelector('.item-1.kind-a')).not.toBeNull();
+			expect(container.querySelector('.item-2.kind-b')).not.toBeNull();
+		});
+
+		it('hydrates three-item list where the middle item has if=false', async () => {
+			await hydrateComponent(
+				ServerComponents.ForIfSwitchWithDisabled,
+				ClientComponents.ForIfSwitchWithDisabled,
+			);
+
+			const items = Array.from(container.querySelectorAll('.item')).map((n) => n.textContent);
+			// item-2 has show=false so only item-1 and item-3 render
+			expect(items).toEqual(['A-1', 'A-3']);
+			expect(container.querySelector('.item-2')).toBeNull();
+		});
+	});
+
+	// ── switch + try (no for/if — baseline for try cursor behaviour) ─────────────
+	describe('switch + try', () => {
+		it('hydrates a switch where the matched case wraps a try/pending', async () => {
+			await hydrateComponent(ServerComponents.SwitchTry, ClientComponents.SwitchTry);
+
+			// async resolves synchronously via Promise.resolve, flush after hydrate
+			await Promise.resolve();
+			expect(container.querySelector('.resolved-a')?.textContent).toBe('A resolved');
+			expect(container.querySelector('.pending-a')).toBeNull();
+		});
+	});
+
+	// ── for + switch + try (no if) ────────────────────────────────────────────────
+	describe('for + switch + try', () => {
+		it('hydrates 2-item for loop where each item uses switch+try', async () => {
+			await hydrateComponent(ServerComponents.ForSwitchTry, ClientComponents.ForSwitchTry);
+
+			const items = Array.from(container.querySelectorAll('.item')).map((n) => n.textContent);
+			expect(items).toEqual(['A-1', 'B-2']);
+		});
+	});
+
+	// ── for + if + try (no switch) ────────────────────────────────────────────────
+	describe('for + if + try', () => {
+		it('hydrates 2-item for loop where each item uses if+try', async () => {
+			await hydrateComponent(ServerComponents.ForIfTry, ClientComponents.ForIfTry);
+
+			const items = Array.from(container.querySelectorAll('.item')).map((n) => n.textContent);
+			expect(items).toEqual(['item-1', 'item-2']);
+		});
+	});
+
+	// ── for + if + switch + try ───────────────────────────────────────────────────
+	describe('for + if + switch + try', () => {
+		it('hydrates single-item for+if+switch+try (no inter-item advance needed)', async () => {
+			await hydrateComponent(
+				ServerComponents.ForIfSwitchTrySingle,
+				ClientComponents.ForIfSwitchTrySingle,
+			);
+
+			const items = Array.from(container.querySelectorAll('.item')).map((n) => n.textContent);
+			expect(items).toEqual(['A-1']);
+		});
+
+		it('hydrates two-item for+if+switch+try (cursor must advance correctly between items)', async () => {
+			await hydrateComponent(
+				ServerComponents.ForIfSwitchTryMulti,
+				ClientComponents.ForIfSwitchTryMulti,
+			);
+
+			const items = Array.from(container.querySelectorAll('.item')).map((n) => n.textContent);
+			expect(items).toEqual(['A-1', 'B-2']);
+			expect(container.querySelector('.item-1.kind-a')).not.toBeNull();
+			expect(container.querySelector('.item-2.kind-b')).not.toBeNull();
+		});
+	});
+});
