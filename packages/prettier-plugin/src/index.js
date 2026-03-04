fee4694be1e7063e6e5cc20a9d17c3ef0137160c
Leonidas <329182+leonidaz@users.noreply.github.com>
2026-03-04 18:56:45 -0500

    add back fallback for typeParameters

--- packages/prettier-plugin/src/index.js
+++ packages/prettier-plugin/src/index.js
@@ -1416,6 +1416,10 @@ function printRippleNode(node, path, options, print, args) {
 			if (node.typeArguments) {
 				parts.push(path.call(print, 'typeArguments'));
 			}
+			// @ts-expect-error account for future changes as our acorn-typescript is buggy
+			else if (node.typeParameters) {
+				parts.push(path.call(print, 'typeParameters'));
+			}
 
 			const argsDoc = printCallArguments(path, options, print);
 			parts.push(argsDoc);
@@ -4108,6 +4112,11 @@ function printNewExpression(node, path, options, print) {
 
 	if (node.typeArguments) {
 		parts.push(path.call(print, 'typeArguments'));
+	}
+	// @ts-expect-error account for future changes as our acorn-typescript is buggy
+	else if (node.typeParameters) {
+		// @ts-expect-error
+		parts.push(path.call(print, 'typeParameters'));
 	}
 
 	if (node.arguments && node.arguments.length > 0) {
