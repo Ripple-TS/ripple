056c9430010d521610404ff31fdd2ac68d4a447a
Jovi De Croock <decroockjovi@gmail.com>
2026-04-22 18:36:21 +0200

    Hoisting helper in core

--- packages/tsrx-react/src/transform.js
+++ packages/tsrx-react/src/transform.js
@@ -17,6 +17,7 @@ import {
 	isInterleavedBody as is_interleaved_body_core,
 	isCapturableJsxChild as is_capturable_jsx_child,
 	captureJsxChild,
+	isHoistSafeJsxNode as is_hoist_safe_jsx_node,
 } from '@tsrx/core';
 
 /**
@@ -892,116 +893,6 @@ function references_scope_bindings(node, scope_bindings) {
 	}
 
 	return false;
-}
-
-/**
- * @param {AST.Literal} node
- * @returns {boolean}
- */
-function is_static_literal(node) {
-	return (
-		node.value === null ||
-		typeof node.value === 'string' ||
-		typeof node.value === 'number' ||
-		typeof node.value === 'boolean' ||
-		typeof node.value === 'bigint'
-	);
-}
-
-/**
- * @param {any} node
- * @returns {boolean}
- */
-function is_hoist_safe_expression(node) {
-	if (!node || typeof node !== 'object') return false;
-
-	switch (node.type) {
-		case 'Literal':
-			return is_static_literal(node);
-		case 'TemplateLiteral':
-			return node.expressions.length === 0;
-		case 'UnaryExpression':
-			return node.operator !== 'delete' && is_hoist_safe_expression(node.argument);
-		case 'BinaryExpression':
-		case 'LogicalExpression':
-			return is_hoist_safe_expression(node.left) && is_hoist_safe_expression(node.right);
-		case 'ConditionalExpression':
-			return (
-				is_hoist_safe_expression(node.test) &&
-				is_hoist_safe_expression(node.consequent) &&
-				is_hoist_safe_expression(node.alternate)
-			);
-		case 'SequenceExpression':
-			return node.expressions.every(is_hoist_safe_expression);
-		case 'ParenthesizedExpression':
-			return is_hoist_safe_expression(node.expression);
-		case 'JSXElement':
-			return is_hoist_safe_jsx_node(node);
-		case 'JSXFragment':
-			return node.children.every(is_hoist_safe_jsx_child);
-		default:
-			return false;
-	}
-}
-
-/**
- * @param {any} node
- * @returns {boolean}
- */
-function is_hoist_safe_jsx_child(node) {
-	if (!node || typeof node !== 'object') return false;
-
-	switch (node.type) {
-		case 'JSXText':
-			return true;
-		case 'JSXElement':
-			return is_hoist_safe_jsx_node(node);
-		case 'JSXFragment':
-			return node.children.every(is_hoist_safe_jsx_child);
-		case 'JSXExpressionContainer':
-			return (
-				node.expression.type !== 'JSXEmptyExpression' && is_hoist_safe_expression(node.expression)
-			);
-		default:
-			return false;
-	}
-}
-
-/**
- * @param {ESTreeJSX.JSXAttribute | ESTreeJSX.JSXSpreadAttribute} attribute
- * @returns {boolean}
- */
-function is_hoist_safe_jsx_attribute(attribute) {
-	if (attribute.type === 'JSXSpreadAttribute') return false;
-	if (attribute.value == null) return true;
-
-	if (attribute.value.type === 'Literal') {
-		return is_static_literal(attribute.value);
-	}
-
-	if (attribute.value.type === 'JSXExpressionContainer') {
-		return (
-			attribute.value.expression.type !== 'JSXEmptyExpression' &&
-			is_hoist_safe_expression(attribute.value.expression)
-		);
-	}
-
-	return false;
-}
-
-/**
- * @param {ESTreeJSX.JSXElement | ESTreeJSX.JSXFragment} node
- * @returns {boolean}
- */
-function is_hoist_safe_jsx_node(node) {
-	if (node.type === 'JSXFragment') {
-		return node.children.every(is_hoist_safe_jsx_child);
-	}
-
-	return (
-		node.openingElement.attributes.every(is_hoist_safe_jsx_attribute) &&
-		node.children.every(is_hoist_safe_jsx_child)
-	);
 }
 
 /**
